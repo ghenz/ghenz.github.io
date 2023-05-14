7d3f974760616b57a5baa392e8825b8e011158ad
Gabriel Henz <gabrielhenz@gmail.com>
2023-05-14 03:05:53 -0300

    chore: change package manager from pnpm to npm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
      <h1>Hey, my name is Gabriel</h1> 
-     <h2>I'm software developer</h2>
+     <h2>Im software developer</h2>
     </main>
   )
 }
